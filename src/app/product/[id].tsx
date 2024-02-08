0ddd9f7ec0849d5ce7b1e572f0609f875bf2955a
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-02-07 23:57:18 -0300

    Feat: Rota Product Id ref

---
+++ src/app/product/[id].tsx
@@ -0,0 +1,12 @@
+import { useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+
+export default function Product() {
+    const { id } = useLocalSearchParams(); 
+
+    return (
+        <View className="flex-1">
+
+        </View>
+    )
+}
