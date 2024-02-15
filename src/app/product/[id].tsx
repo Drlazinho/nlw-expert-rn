c2e9e9a965682add8d9dbafabc987740135156ef
Lázaro Pimentel <118478127+LazaroAmvox@users.noreply.github.com>
2024-02-15 01:15:09 -0300

    add Product Cart - State Global with "Zustand"

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Image, Text, View } from "react-native";
 
 import { PRODUCTS } from "@/utils/data/products";
@@ -7,10 +7,20 @@ import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons";
 import { LinkButton } from "@/components/link-button";
 
+import { useCartStore } from "@/stores/cart-store";
+
 export default function Product() {
+    const cartStore = useCartStore();
+    const navigation = useNavigation()
     const { id } = useLocalSearchParams(); 
 
     const product = PRODUCTS.filter((item) => item.id === id)[0];
+    console.log(cartStore.products)
+
+    function handleAddToCart() {
+        cartStore.add(product)
+        navigation.goBack()
+    }
 
     return (
         <View className="flex-1">
@@ -28,7 +38,7 @@ export default function Product() {
             </View>
 
             <View className="p-5 pb-8 gap-5">
-                <Button>
+                <Button onPress={handleAddToCart}>
                     <Button.Icon>
                         <Feather name="plus-circle" size={20}/>
                     </Button.Icon>
---
+++ src/stores/helpers/cart-in-memory.ts
@@ -0,0 +1,12 @@
+import { ProductProps } from "@/utils/data/products";
+import { ProductCardProps } from "../cart-store";
+
+export function add(products: ProductCardProps[], newProduct: ProductProps) {
+    const existingProduct = products.find(({id}) => newProduct.id === id);
+
+    if(existingProduct) {
+        return products.map((product) => existingProduct.id === existingProduct.id ? {...product, quantity: product.quantity + 1} :  product)
+    }
+
+    return [...products, { ...newProduct, quantity: 1 }]
+}
