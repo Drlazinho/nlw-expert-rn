44bdcc6aab82672d690898de86a01b73eb7ee4ba
Lázaro Pimentel <118478127+LazaroAmvox@users.noreply.github.com>
2024-03-02 18:46:11 -0300

    feat: Ajuste Redirect No Product

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import { View, FlatList, SectionList, Text } from "react-native";
 
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
@@ -13,7 +13,7 @@ export default function Home() {
   const cartStore = useCartStore()
   const [category, setCategory] = useState(CATEGORIES[0]);
 
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
   const cartQuantityItems = cartStore.products.reduce((total, product)=> total + product.quantity, 0);
 
--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,6 @@
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Image, Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
@@ -10,45 +11,62 @@ import { LinkButton } from "@/components/link-button";
 import { useCartStore } from "@/stores/cart-store";
 
 export default function Product() {
-    const cartStore = useCartStore();
-    const navigation = useNavigation()
-    const { id } = useLocalSearchParams(); 
+  const cartStore = useCartStore();
+  const navigation = useNavigation();
+  const { id } = useLocalSearchParams();
 
-    const product = PRODUCTS.filter((item) => item.id === id)[0];
-    console.log(cartStore.products)
+  const product = PRODUCTS.find((item) => item.id === id);
 
-    function handleAddToCart() {
-        cartStore.add(product)
-        navigation.goBack()
+  function handleAddToCart() {
+    if (product) {
+      cartStore.add(product);
+      navigation.goBack();
     }
+  }
 
-    return (
-        <View className="flex-1">
-            <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
+  if (!product) {
+    return <Redirect href="/" />;
+  }
 
-            <View className="p-5 mt-8 flex-1">
-                <Text className="text-lime-400 text-2xl font-heading my-2">{formatCurrency(product.price)}</Text>
-                <Text className="text-slate-400 font-body text-base leading-6 mb-6">{product.description}</Text>
+  return (
+    <View className="flex-1">
+      <Image
+        source={product.cover}
+        className="w-full h-52"
+        resizeMode="cover"
+      />
 
-                {
-                    product.ingredients.map((ingredient) => (
-                        <Text key={ingredient} className="text-slate-400 font-body text-base leading-6">{"\u2022"} {ingredient}</Text>
-                    ))
-                }
-            </View>
 
-            <View className="p-5 pb-8 gap-5">
-                <Button onPress={handleAddToCart}>
-                    <Button.Icon>
-                        <Feather name="plus-circle" size={20}/>
-                    </Button.Icon>
-                    <Button.Text>
-                        Adicionar ao pedido
-                    </Button.Text>
-                </Button>
+      <View className="p-5 mt-8 flex-1">
+      <Text className="text-white text-xl font-heading">{product.title}</Text>
 
-                <LinkButton title="Voltar ao cardápio" href="/"/>
-            </View>
-        </View>
-    )
+        <Text className="text-lime-400 text-2xl font-heading my-2">
+          {formatCurrency(product.price)}
+        </Text>
+        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+          {product.description}
+        </Text>
+
+        {product.ingredients.map((ingredient) => (
+          <Text
+            key={ingredient}
+            className="text-slate-400 font-body text-base leading-6"
+          >
+            {"\u2022"} {ingredient}
+          </Text>
+        ))}
+      </View>
+
+      <View className="p-5 pb-8 gap-5">
+        <Button onPress={handleAddToCart}>
+          <Button.Icon>
+            <Feather name="plus-circle" size={20} />
+          </Button.Icon>
+          <Button.Text>Adicionar ao pedido</Button.Text>
+        </Button>
+
+        <LinkButton title="Voltar ao cardápio" href="/" />
+      </View>
+    </View>
+  );
 }
